86a9ea766a77863b263a0f805e8108422248a878
choish <wadolsh@gmail.com>
2013-03-26 16:25:35 +0900

    save

---
+++ ykc_gae/war/p/js/vars.js
@@ -0,0 +1,7 @@
+var mongolab_url = "https://api.mongolab.com/api/1";
+var mongolab_db_url = mongolab_url + "/databases/ykcsys";
+var s43_doc_url_base = mongolab_db_url + "/collections/s43";
+var apiKey = "50ee07c6e4b0a0d1d01344f3";
+var s43_doc_url = s43_doc_url_base + "?apiKey=" + apiKey;
+var kendo_meta_data_doc_url = mongolab_db_url + "/collections/kendo_meta_data?apiKey=" + apiKey;
+var _id_key = "_id";
