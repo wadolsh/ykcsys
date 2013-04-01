f0717fb7ee730b652156f41270e15624d7314692
SUNGHO CHOI <wadolsh@yahoo.co.jp>
2013-04-01 04:12:04 -0400

    save

---
+++ node.js/server.js
@@ -0,0 +1,49 @@
+// reference the http module so we can create a webserver
+var http = require("http");
+var fs = require('fs');
+
+// create a server
+http.createServer(function(req, res) {
+    //console.log(req.headers);
+    // on every request, we'll output 'Hello world'
+    //res.end("Hello world from Cloud9!---");
+    fs.readFile('ykc_gae/war' + req.url, function(error, data) {
+        var n = null;
+        var ext = null;
+        if ((n = req.url.lastIndexOf(".")) != -1) {
+            //console.log(ext);
+            switch (req.url.substring(n)) {
+                case '.js' :
+                    res.writeHead(200, {'Content-Type': 'text/javascript'});
+                    break;
+                case '.css' :
+                    res.writeHead(200, {'Content-Type': 'text/css'});
+                    break;
+                case '.gif' :
+                    res.writeHead(200, {'Content-Type': 'image/gif'});
+                    break;
+                case '.png' :
+                    res.writeHead(200, {'Content-Type': 'image/png'});
+                    break;
+                case '.jpg' :
+                    res.writeHead(200, {'Content-Type': 'image/jpeg'});
+                    break;
+                default :
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                
+            }
+        }
+
+        //res.writeHead(200, {'Content-Type': 'text/html'});
+        //res.writeHead(200);
+        res.end(data);
+    });
+    
+    
+}).listen(process.env.PORT, process.env.IP);
+
+// Note: when spawning a server on Cloud9 IDE, 
+// listen on the process.env.PORT and process.env.IP environment variables
+
+// Click the 'Run' button at the top to start your server,
+// then click the URL that is emitted to the Output tab of the console
